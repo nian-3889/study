297ce238c89bb956cc680825f47586f8e577085b
Nian <94526114+nian-3889@users.noreply.github.com>
2022-04-02 14:55:37 +0900

    Create 3. typeof.js

---
+++ javascript/3. typeof.js
@@ -0,0 +1,18 @@
+//string : 문자열
+//문자열은 따옴표('' 혹은 "")로 감싸야 함
+let hi = '안녕하세요'
+document.write(typeof(hi))
+
+//Number : 숫자
+//자바스크립트는 큰 숫자를 저장하는 BigInt라는 자료형을 지원함.
+//그 수가 크거나 작은 경우 지수(e)표현으로 바꿀 수 있음.
+//사칙연산은 연산자.js에서 확인요망
+let num = 1
+document.write(typeof(num))
+
+//boolean : 불리언 혹은 불 자료형. 쉽게 말하자면 참 거짓만 있는 형태.
+//true(참)와 false(거짓)로 구성.
+let eat = true
+let sleep = false
+document.write(typeof(eat))
+document.write(typeof(sleep))
