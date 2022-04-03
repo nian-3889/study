9ccde68481efc52af504f714e06e3cc9c2482de5
Nian <94526114+nian-3889@users.noreply.github.com>
2022-04-03 12:02:10 +0900

    Update 7. if.js

--- javascript/7. if.js
+++ javascript/7. if.js
@@ -8,8 +8,6 @@ if(조건식) {
     실행 식1
 } else if(조건식) {
     실행 식2
-} ... {
-    실행식 ...
 } else {
     실행식
 }`)
@@ -21,6 +19,24 @@ if (x >= 100) {
     document.write("x의 값은 100이 넘습니다.")
 } else {
     document.write("x의 값은 100을 넘지 않습니다.")
+}
+
+//else if를 포함한 예시
+
+x = 89
+
+if (x === 100) {
+    console.log("최고 등급을 받았습니다.")
+} else if (x >= 90) {
+    console.log("A등급을 받았습니다.")
+} else if (x >= 80) {
+    console.log("B등급을 받았습니다.")
+} else if (x >= 70) {
+    console.log("C등급을 받았습니다.")
+} else if (x >= 40) {
+    console.log("D등급을 받았습니다")
+} else if (x < 40) {
+    console.log("낙제점을 받았습니다")
 }
 
 //무조건 false로 취급하는 값이 있다.
