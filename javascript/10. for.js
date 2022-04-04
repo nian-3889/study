9e31bee34129dce2a29e072887923c4a2a85de9c
Nian <94526114+nian-3889@users.noreply.github.com>
2022-04-04 11:22:31 +0900

    Create 10. for.js

---
+++ javascript/10. for.js
@@ -0,0 +1,20 @@
+//어떤 것을 반복적으로 시행할 때 쓰는 문법
+//while은 11. while.js를 확인
+
+//for문의 기본 구성은 아래와 같습니다.
+document.write(`for([초기문];[조건문];[증감문])
+                    실행문
+                *변수명을 const로 할 경우, 에러가 나므로 주의`)
+
+//예시
+for(let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+//이중 for문
+
+
+//for in문
+
+
+//for of문
