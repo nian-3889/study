8cb6a974f7b18cd7cf438dc3d7275d578980d1a8
S. H. J <cielmint9560@gmail.com>
2022-04-05 22:29:15 +0900

    Update 2. 변수 타입.py

--- python/2. 변수 타입.py
+++ python/2. 변수 타입.py
@@ -16,3 +16,14 @@ a = True
 b = False
 print(type(a))
 print(type(b))
+
+#input()함수로 받은 데이터의 자료형
+print(type(input())) #무조건 문자형(str)으로 나옴
+
+#input()함수의 데이터 변환하고 싶다면?
+number = input("숫자를 입력해주세요")
+print(int(number))
+print(float(number)) #실수를 정수로 변환할땐 에러나니 주의
+
+#자바스크립트와 다르게 변환할 수 없는 자료형이 나올 땐 에러가 난다
+#자바스크립트는 NaN이 있는데... 흐규규규...
