a36438fec1656d0406438763a756f4832bec9a48
S. H. J <cielmint9560@gmail.com>
2022-04-05 22:07:10 +0900

    Create 2. 변수 타입.py

---
+++ python/2. 변수 타입.py
@@ -0,0 +1,18 @@
+#문자형(string)
+#자세한 건 1. 변수와 문자열.py 참조
+name = 'Nian'
+print(type(name))
+
+#숫자형. int와 float
+x = 10 #int는 정수
+y = 20.147 #float는 실수
+z = 0.52273e2 #지수표현식. float에 해당
+print(type(x))
+print(type(y))
+print(type(z))
+
+#boolean. true와 false로 나뉨
+a = True
+b = False
+print(type(a))
+print(type(b))
