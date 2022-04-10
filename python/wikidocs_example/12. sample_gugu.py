63487af376a9d4be7f909756050ee340aaf4b9a9
Nian <94526114+nian-3889@users.noreply.github.com>
2022-04-10 10:41:27 +0900

    Create 12. sample_gugu.py

---
+++ python/wikidocs_example/12. sample_gugu.py
@@ -0,0 +1,13 @@
+from cgitb import reset
+from unittest import result
+
+
+def GuGu(n):
+    result = []
+    i = 1
+    while i < 10:
+        result.append(n * i)
+        i += 1
+    return result
+
+print(GuGu(2))
